adb2b7f98eaf7770adfe3516af4b658d8f3bc037
SquareX <squarex@clairede-macbook-air.local>
2017-02-20 16:09:17 +0800

    Initial Commit

---
+++ (作業2)模仿Instagram畫面/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  (作業2)模仿Instagram畫面
+//
+//  Created by SquareX on 2017/2/20.
+//  Copyright © 2017年 SquareX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ (作業2)模仿Instagram畫面Tests/___2___Instagram__Tests.swift
@@ -0,0 +1,36 @@
+//
+//  ___2___Instagram__Tests.swift
+//  (作業2)模仿Instagram畫面Tests
+//
+//  Created by SquareX on 2017/2/20.
+//  Copyright © 2017年 SquareX. All rights reserved.
+//
+
+import XCTest
+@testable import ___2___Instagram__
+
+class ___2___Instagram__Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
